9fea8dea6683a2e02b4bac5ac052df42710a6504
Apple <apple@dhcp017104094121.corp.apple.com>
2018-11-21 10:16:06 -0600

    added keys.json gitignore and modded fetcher

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # chirpfetch
 Chirpfetch is a tweet fetching app.
+
+## keys.json
+
+Only store the authentication keys in the keys.json file as strings like so:
+
+```json
+{
+	"consumer_key": "xxxx",
+	"consumer_secret": "xxxx",
+	"access_token_key": "xxxx",
+	"access_token_secret": "xxxx"
+}
+```
--- fetcher.js
+++ fetcher.js
@@ -1,41 +1,41 @@
-const Twitter = require("twitter")
-const auth = {
-  consumer_key: "",
-  consumer_secret: "",
-  access_token_key: "",
-  access_token_secret: ""
-}
-const twitters = ["gvb_actueel", "htm_reisinfo", "retrotterdam"]
+const fs = require( "fs" )
+const Twitter = require( "twitter" )
 
-function fetcher(auth, twitters, amount) {
+const twitters = [ "gvb_actueel", "htm_reisinfo", "retrotterdam" ]
 
-  const client = new Twitter({
-    consumer_key: auth.consumer_key,
-    consumer_secret: auth.consumer_secret,
-    access_token_key: auth.access_token_key,
-    access_token_secret: auth.access_token_secret
+function fetcher( twitters, amount ) {
+  fs.readFile( "keys.json", "utf8", ( error, data ) => {
+    if ( error ) throw error
+    keys = JSON.parse( data )
+
+    const client = new Twitter( keys )
+
+    for ( let handler = 0; handler < twitters.length; handler++ ) {
+      let data = []
+      client.get( `statuses/user_timeline.json?screen_name=${ twitters[handler] }&count=${ amount }`, ( error, tweets, response ) => {
+        if (!error) {
+          for ( let post in tweets ) {
+            data.push( {
+              id: tweets[post].id,
+              created_at: tweets[post].created_at,
+              screen_name: tweets[post].user.screen_name,
+              text: tweets[post].text,
+              tweetUrl: `https://twitter.com/${ tweets[post].user.screen_name }/status/${ tweets[post].id_str }`,
+              incidents: {}
+            } )
+          }
+        }
+        console.log( data )
+      })
+    }
   })
 
-  for (let handler = 0; handler < twitters.length; handler++) {
-    let data = []
-    client.get(`statuses/user_timeline.json?screen_name=${twitters[handler]}&count=${amount}`, (error, tweets, response) => {
-      if (!error) {
-        for (let post in tweets) {
-          data.push({
-            id: tweets[post].id,
-            created_at: tweets[post].created_at,
-            screen_name: tweets[post].user.screen_name,
-            text: tweets[post].text,
-            tweetUrl: `https://twitter.com/${tweets[post].user.screen_name}/status/${tweets[post].id_str}`,
-            incidents: {}
-          })
-        }
-      }
-      console.log(data)
-    })
-  }
+
 }
 
-fetcher(auth, twitters, 5)
+// const client = new Twitter( auth )
+
+
+fetcher( twitters, 5 )
 
 exports.module = fetcher
